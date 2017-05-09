cdc96a174f8943466cfeda961d0709e132161909
BRFlores <bryan.flores@epn.edu.ec>
2017-05-09 16:53:41 -0500

    ultima clase

--- Harold javascript/app.js
+++ Harold javascript/app.js
@@ -2,3 +2,23 @@
  * Created by USRDEL on 5/3/17.
  */
 console.log('hola');
+var harold= {
+
+    nombre: 'Harold',
+    apellido: 'Flores',
+    fechaNacimiento: new Date(),
+    imprimir: function (){
+    console.log(this.nombre, this.apellido);
+},
+    anio:function(){
+        return this.fechaNacimiento.getFullYear()
+    },
+    diferenciaEdad: function (edadAComparar) {
+        return Math.abs(this.anio() - edadAComparar)
+
+    }
+}
+harold.imprimir();
+console.log(harold.imprimir())
+console.log(harold.anio());
+console.log(harold.diferenciaEdad(2013));
